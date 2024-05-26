47143d1fa34cbe7d2f448bdf594c64a2bd156353
Nestor Qin <imba.qxy@gmail.com>
2024-05-26 17:28:52 -0400

    static build?

---
+++ docs/_next/static/x7kiAH36jFprwgwxLqWgr/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-a28ccea19a99e845.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
